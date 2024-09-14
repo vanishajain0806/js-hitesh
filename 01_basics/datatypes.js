478f0e4ce3bcb7ee99e811902f41c0e75fa45ed9
Vanisha Jain <71638122+vanishajain0806@users.noreply.github.com>
2024-09-14 20:41:26 +0000

    variables and datatypes done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,2 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3) //not allowed because we are using nodejs not browser
---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 7447
+let accountEmail = "vanisha@gmail.com"
+var accountPassword = "123456" // not preferable because var doesn't contain block scope or functional scope
+accountCity = "Lucknow" // possible but not preferable
+
+let name; //returns undefined
+
+//only const variables can't be changed
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,name]);
+accountEmail = "vj@google.com"
+accountPassword = "1212121"
+accountCity = "Kanpur"
+console.table([accountEmail,accountPassword,accountCity]);
+
