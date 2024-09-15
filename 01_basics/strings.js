e61f1ab80a52f22ff915681eade18d9a8e9da503
Vanisha Jain <71638122+vanishajain0806@users.noreply.github.com>
2024-09-15 11:19:02 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,12 @@
+const name = "Vanisha"
+const repoCount = 5
+
+console.log("Hello " + name + " with " + repoCount + " RepoCount"); // outdated way not preferable
+
+// String Interpolation:(we can use the methods of strings in this as well)
+console.log(`Hello my name is ${name} and my RepoCount is ${repoCount}`);
+
+// Another way to declare String:
+const userName = new String('Vanisha_74')
+// It provides a key value pair:
+console.log(userName[1]); //a
