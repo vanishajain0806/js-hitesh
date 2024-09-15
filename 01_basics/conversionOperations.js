8fecf259b23d7cc7b6b0e2eb27faf053e2c97fe4
Vanisha Jain <71638122+vanishajain0806@users.noreply.github.com>
2024-09-15 10:56:21 +0000

    modified Operations

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,46 @@
+let score = 33
+//both are correct
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let score1 = "33"
+let valueInNumber = Number(score1) // type conversion me String Boolean kuch bhi kr sakte hai.
+//console.log(typeof(valueInNumber));
+
+let score2 = "33abc"
+let valueInNumber1 = Number(score2)
+// console.log(typeof(valueInNumber1));
+// console.log(valueInNumber1);
+
+// "33" -> 33
+// "33abc" -> NaN
+// null -> 0
+// undefined -> undefined
+// true -> 1; false -> 0
+
+// ********************** Operations ************************* //
+let val = 7
+let negVal = -val
+// console.log(negVal);
+let str1 = "hello"
+let str2 = " Vanisha"
+// console.log(str1+str2);
+
+// console.log("1"+2);// 12
+// console.log(1+"2");// 12
+// console.log(1+2+"3");// 33
+// console.log("1"+2+3);// 123
+
+// console.log(+true); // + is used to convert the value in Number
+// console.log(+""); // not preferable
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
