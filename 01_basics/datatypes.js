ed721b4735eda634359fb82dee2d4fee866be5cb
Vanisha Jain <71638122+vanishajain0806@users.noreply.github.com>
2024-09-15 09:21:14 +0000

    datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,2 +1,24 @@
 "use strict"; // treat all JS code as newer version
 // alert(3+3) //not allowed because we are using nodejs not browser
+let name = "Vanisha"
+let age = "23"
+let isLoggedIn = false
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+//Primitive DataTypes:
+//number -> range(2^53)
+//bigint
+//string -> "" or ''
+//boolean -> true/false
+//null -> standalone value(representation of empty value)
+//undefined -> value not yet assigned
+//symbol -> to find the uniqueness
+
+//object: It is a datatype
+
+
+
+
+
