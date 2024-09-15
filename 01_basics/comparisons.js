d302e96980508a831f9402174d423bb3f8f968c4
Vanisha Jain <71638122+vanishajain0806@users.noreply.github.com>
2024-09-15 10:56:05 +0000

    modified comparisions

---
+++ 01_basics/comparisons.js
@@ -0,0 +1,7 @@
+// console.log("2">1); // doesn't give predictable value
+
+// console.log(null > 0);// comparison operator changes null to number that's why output->false
+// console.log(null == 0);// equality doesn't change null to number // output->false
+// console.log(null >= 0);// true
+
+console.log("2" === 2) // === is a strict check, it compares not only the value but the datatype also.
