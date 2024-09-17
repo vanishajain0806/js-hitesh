587b5ece68712c31913a895783722be541667dba
Vanisha Jain <71638122+vanishajain0806@users.noreply.github.com>
2024-09-17 11:12:27 +0000

    arrays done

---
+++ 02_basics/arrays02.js
@@ -0,0 +1,29 @@
+const myArr = ["Vanisha","Harhsita","Vaibhav"]
+const myArr2 = ["Fruity","Chinki","Golu"]
+
+// myArr.push(myArr2);
+// console.log(myArr);
+
+// const newArr = myArr.concat(myArr2);
+// console.log(newArr);
+
+// const newArr2 = [...myArr,...myArr2]//spread method...same as concat
+// console.log(newArr2);
+
+// const arr = [1,2,3,[4,5],6,[7,9,8,[6,9,0]],1,2];
+// const arr2 = arr.flat(Infinity)//it will flat all the indepth arrays...Infinity shows the no of depth, you can specify any number as well
+// console.log(arr2);
+
+// console.log(Array.isArray("vanisha"));//returns false
+// console.log(Array.from("Vanisha"));//converts it in Array.
+// console.log(Array.from({name:"Vanisha"})); //intersting
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+// console.log(Array.of(score1,score2,score3));
+
+
+
+
+
