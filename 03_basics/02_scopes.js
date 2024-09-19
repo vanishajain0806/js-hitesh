f7098e902c8cc1035977ce4d62dd0c4ae311c2d2
Vanisha Jain <71638122+vanishajain0806@users.noreply.github.com>
2024-09-19 11:23:29 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,11 @@
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+// console.log(a);//error
+// console.log(b);//error
+console.log(c);//30...this is the reason why we don't use the var!
+
+// console pe global scope aur vs code me node use krne pe gloabal scope are different
