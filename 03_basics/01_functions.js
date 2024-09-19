5416186ee7bf0994418dffc1dff459351059d3cb
Vanisha Jain <71638122+vanishajain0806@users.noreply.github.com>
2024-09-19 10:26:07 +0000

    functions basics done

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,70 @@
+//function definition:
+// function sayMyName(){
+//     console.log("V");
+//     console.log("A");
+//     console.log("N");
+//     console.log("I");
+//     console.log("S");
+//     console.log("H");
+//     console.log("A");
+// }
+// sayMyName//reference of function
+// sayMyName()//execution
+
+// function add(num1,num2){
+//     console.log(num1+num2);
+// }
+// add(7,4)//11
+// const ans = add(7,4)
+// console.log(result);//undefined
+
+// function add(num1,num2){
+//     let ans = num1+num2
+//     return ans
+// }
+// const result = add(7,4)
+// console.log("Result:" ,result);
+
+// function loginUserMessage(username)/*(username = "sam") if you don't want to send undefined as parameters*/{
+//     if(username === undefined)/*(!username)*/{
+//         console.log("Please enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+// console.log(loginUserMessage("Vanisha"));//Vanisha
+// console.log(loginUserMessage());//undefined
+
+// how to send multiple values in the functions parameters
+// function calculateCartPrice(...num1)/*...num1 is a rest operator used to send multiple values in a single variable*/{
+//     return num1
+// }
+// console.log(calculateCartPrice(200,300,400,20000));
+
+// how to pass object as parameter in the function:
+// 1st way:
+// const user = {
+//     name:"Vanisha",
+//     age:23
+// }
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.name} and age is ${anyobject.age}`);
+// }
+// handleObject(user)
+// 2nd way:
+// handleObject({
+//     name:"Vanisha",
+//     age:23
+// })
+
+// how to pass arrays as parameters in the function:
+// const myArr = [200,400,600,800]
+
+// function returnSecondValue(getArray){
+//     return getArray[1]
+// }
+// console.log(returnSecondValue(myArr));
+
+
+
+
