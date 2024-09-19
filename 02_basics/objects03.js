e1ce1076416e4a4c5e73ea297b797a7f37fc6ec4
Vanisha Jain <71638122+vanishajain0806@users.noreply.github.com>
2024-09-19 09:23:25 +0000

    objects3 done

---
+++ 02_basics/objects03.js
@@ -0,0 +1,24 @@
+// how to destructure the object:
+const course = {
+    coursename:"js-hitesh",
+    price:999,
+    courseInstructor:"hitesh"
+}
+const{courseInstructor} = course
+console.log(courseInstructor);
+//the above thing can be done like this also...when we want to change the name of the key:
+const{courseInstructor:Instructor} = course
+console.log(Instructor);
+
+//react ki baatein:
+// const navabr = ({company}) => {
+// }//rather than using (props.company) directly use{company}//this is how destructuring is done
+
+// navabr(company = "vanisha")
+
+//json format:
+// {
+//     "name":"vanisha",
+//     "coursename":"js-hitesh",
+//     "price":"free"
+// }
