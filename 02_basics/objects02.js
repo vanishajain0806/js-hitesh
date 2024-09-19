420b1053183d90d5a9f7234b1dc53c961fedf526
Vanisha Jain <71638122+vanishajain0806@users.noreply.github.com>
2024-09-19 08:40:09 +0000

    objects part 2 done

--- 02_basics/objects02.js
+++ 02_basics/objects02.js
@@ -22,6 +22,51 @@ const regularUser = {
 
 // console.log(regularUser.fullname.userfullname.firstname);
 
-// console.log(regularUser.fullname?.userfullname.firstname); // ? is used to check if there exists any full name then only print
+// console.log(regularUser.fullname?.userfullname.firstname); // ? is used to check if there exists any full name then only print//when the respenose is coming from an api
+
+//to merge the objects:
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+//1st way:
+//const obj3 = {obj1,obj2}//problametic
+//2nd way:
+const obj3 = Object.assign(obj1,obj2)//here obj1 is the target and obj2 is the source...basically whatever is written in the first place will be the target and rest will be source i.e. Object.assign({},obj1,obj2) then {} would be the target
+// console.log(obj3);
+// console.log(obj1);
+// console.log(obj3===obj1);//true only if there isn't any {} is used
+//3rd way:
+const obj4 = {...obj1,...obj2}//spread all the objects
+//console.log(obj3);
+//if the objects are coming from the databases:
+const users = [
+    {
+        id:1,
+        email:"v@gmail.com"
+    },
+    {
+        id:1,
+        email:"v@gmail.com"
+    },
+    {
+        id:1,
+        email:"v@gmail.com"
+    }
+]//when fetching from database the objects come in array
+users[1].email//this is how to fetch the data
+
+//console.log(tinderUser);
+
+//console.log(Object.keys(tinderUser));//the return type of this is an array//we can iterate over the array...we can use loop through the array
+
+//console.log(Object.values(tinderUser));
+//console.log(Object.entries(tinderUser));
+
+//to check if the key exist in the object or not
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));//true
+
+
+
+
 
 
