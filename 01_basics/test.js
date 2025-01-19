f89bb0bb543793a6e4c79e76b5e941116bc8ca22
Vanisha Jain <71638122+vanishajain0806@users.noreply.github.com>
2025-01-19 08:11:59 +0000

    added some instructions for node

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,2 @@
+// to install node in the system: top left corner->view->command palette->type container->select add dev container->type node-> select node & JS->version default->ok->rebuild.
 console.log("Vanisha")
