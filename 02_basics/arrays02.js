943b9f876bbe2d31ca3b891ea0a75963bd95b09b
Vanisha Jain <71638122+vanishajain0806@users.noreply.github.com>
2025-02-05 05:25:48 +0000

    important comments added

--- 02_basics/arrays02.js
+++ 02_basics/arrays02.js
@@ -2,26 +2,26 @@ const myArr = ["Vanisha","Harhsita","Vaibhav"]
 const myArr2 = ["Fruity","Chinki","Golu"]
 
 // myArr.push(myArr2);
-// console.log(myArr);
+// console.log(myArr);// this will push the elements of myArr2 into myArr as a whole array not like element by element.//pushes in the existing array
 
 // const newArr = myArr.concat(myArr2);
-// console.log(newArr);
+// console.log(newArr);//this returns a new array//properly both the arrays can be merged using this concat method.
 
 // const newArr2 = [...myArr,...myArr2]//spread method...same as concat
-// console.log(newArr2);
+// console.log(newArr2);//can merge more than two arrays.
 
 // const arr = [1,2,3,[4,5],6,[7,9,8,[6,9,0]],1,2];
 // const arr2 = arr.flat(Infinity)//it will flat all the indepth arrays...Infinity shows the no of depth, you can specify any number as well
-// console.log(arr2);
+// console.log(arr2);//means it will remove the depths and concatenate all the elements as a 1D array.
 
-// console.log(Array.isArray("vanisha"));//returns false
+// console.log(Array.isArray("vanisha"));//returns false//it is asking whether "vanisha" is an array
 // console.log(Array.from("Vanisha"));//converts it in Array.
-// console.log(Array.from({name:"Vanisha"})); //intersting
+// console.log(Array.from({name:"Vanisha"})); //intersting//returns an empty array because it was not possible for it to convert into array.
 
 // let score1 = 100
 // let score2 = 200
 // let score3 = 300
-// console.log(Array.of(score1,score2,score3));
+// console.log(Array.of(score1,score2,score3));//returns a new array from the set of elements.
 
 
 
