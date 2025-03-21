719923103d8a1bd59480c403dc971a4554b36f0e
Vanisha Jain <71638122+vanishajain0806@users.noreply.github.com>
2025-03-21 11:34:37 +0000

    added extra conditions of truthy and falsy values

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,12 +1,21 @@
 // truthy values: "0", 'false', " ", [], {}, function(){}
-// false values: false, 0, -0, BigInt 0n, "", null, undefined, NaN
+// falsy values: false, 0, -0, BigInt 0n, "", null, undefined, NaN
 
+// this is how we check if the array is empty
 const userEmail = []
 if(userEmail.length===0){
     console.log("Array is Empty")
 }
 
+// this is how we check if the object is empty
 const emptyObj = {}
 if(Object.keys(emptyObj).length===0){
     console.log("Object is Empty")
 }
+
+// false == 0 //true
+// false == '' //true
+// 0 == '' //true
+
+
+
