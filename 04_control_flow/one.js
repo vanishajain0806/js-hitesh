4544f26249878f72fcb509cb9f3052631a430d70
Vanisha Jain <71638122+vanishajain0806@users.noreply.github.com>
2025-03-21 11:33:53 +0000

    added extra info about (??) operator

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,8 +1,13 @@
-//same as other languages.
-// Nullish Coalescing Operator(??): null undefined
+//if/else statements are just same as other languages.
+
+// Nullish Coalescing Operator(??): this is all about null undefined...it is used to check the values coming from database basically it tells if we want to assign any values at the place of any errors then what we can put and how. Major use in the database.
 
 let val1;
-val1 = null??10
+//val1 = 5??10 // 5
+val1 = null??10 //10
+//val1 = undefined??10 // 10
+//val1 = undefined ?? 10 ?? 20 // 10
 console.log(val1);
 
-// Terniary Operator:  condition?true:false
+// Terniary Operator:  
+// condition?true:false
