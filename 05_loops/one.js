af45603f5510ef08709ddaa7bcdf56a03822a8af
Vanisha Jain <71638122+vanishajain0806@users.noreply.github.com>
2025-04-04 06:00:21 +0000

    comment updated

--- 05_loops/one.js
+++ 05_loops/one.js
@@ -1,4 +1,5 @@
 // for and while loops are same as other languages.
+// in javascript there is no error as out of bound, if loop goes out of bound it will print undefined as the output
 // Array specific loops: 
 // for of:
 const arr = [1,2,3,4,5,6,7,8,9,10]
