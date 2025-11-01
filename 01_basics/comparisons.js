bbafe30ee4b8490608f7c2165e2b8a294dc41df1
Vanisha Jain <71638122+vanishajain0806@users.noreply.github.com>
2025-11-01 11:29:12 +0000

    added a comment

--- 01_basics/comparisons.js
+++ 01_basics/comparisons.js
@@ -1,6 +1,6 @@
-// console.log("2">1); // doesn't give predictable value
+ // console.log("2">1); // doesn't give predictable value
 
-// console.log(null > 0);// comparison operator changes null to number that's why output->false
+// console.log(null > 0);// comparison operator changes null to number(treating it as 0) that's why output->false
 // console.log(null == 0);// equality doesn't change null to number // output->false
 // console.log(null >= 0);// true
 
